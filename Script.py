d9f70345238ce2697dface37e7b9aea50d27db82
Luckyboy27 <86392204+Luckyboy27@users.noreply.github.com>
2021-06-26 16:19:41 +0530

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,27 @@
+class script(object):
+
+    START_TEXT = """<b>Hai ,
+    
+I'm A simple Zee5 link downloader bot With Permanent Thumbnail Support💯.
+Please send me any Zee5 link, I can upload it to telegram as File/Video.
+This is the first bot created by us 🥺, If found any bugs,plx report @innocentbots !
+/start - Check if bot is alive
+/help - To know how the bot works
+/upgrade - Nothing much here
+/showthumb - Shows saved thumbnail
+/delthumb - Clear saved thumbnail
+Click <i>/help</i> for more details....</b>"""
+
+
+    HELP_USER = """<b>Hai, Follow these steps..</b>
+ 
+1. Send Custom Thumbnail (It will be saved permenantly!)
+2. Send your zee5 url and select desired option.
+NOTE: Download may take some time! So please wait for it to complete!"""
+
+
+    ABOUT_TEXT = """⭕️<b>My Name : Innocent Zee5 Bot</b>
+⭕️<b>Creater :</b> @meizinnocent
+⭕️<b>Language :</b> <code>Python3</code>
+⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+⭕️<b>Source Code :</b> 👉 <a href='https://github.com/Luckyboy27/innocentzee5Bot'>Click Here</a>"""
